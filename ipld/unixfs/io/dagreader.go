ed66e35154f05ce4fc0e0b929b711b3d54fd382b
Mo-RIZK <137156501+Mo-RIZK@users.noreply.github.com>
2025-08-07 20:22:56 +0200

    Update dagreader.go

--- ipld/unixfs/io/dagreader.go
+++ ipld/unixfs/io/dagreader.go
@@ -669,8 +669,8 @@ func (dr *dagReader) WriteNWI(w io.Writer) error {
 	//update the indexes and times by retrieving the first set completely
 	dr.RetrieveAllSet(dr.startOfNext, s)
 	//launch a gourotine in the background that do timer and update the times, indexes
-	//go dr.startTimer(ctxx, s)
-	go dr.startTimer2(ctxx, s)
+	go dr.startTimer(ctxx, s)
+	//go dr.startTimer2(ctxx, s)
 	err := dr.WriteNWI2(w, cancell)
 	return err
 
@@ -982,7 +982,7 @@ func (dr *dagReader) RetrieveAllSetAlt(next int, s int) {
 }
 
 func (dr *dagReader) startTimer(ctx context.Context, s int) {
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 
 	for {
