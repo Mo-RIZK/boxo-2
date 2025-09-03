337bf86447181449005e285ba2476a1d1d4195a5
Mo-RIZK <137156501+Mo-RIZK@users.noreply.github.com>
2025-09-03 10:46:51 +0200

    Update blockservice.go

--- blockservice/blockservice.go
+++ blockservice/blockservice.go
@@ -282,12 +282,12 @@ func getBlock(ctx context.Context, c cid.Cid, bs BlockService, fetchFactory func
 	if err != nil {
 		return nil, err
 	}
-	if ex := bs.Exchange(); ex != nil {
+	/*if ex := bs.Exchange(); ex != nil {
 		err = ex.NotifyNewBlocks(ctx, blk)
 		if err != nil {
 			return nil, err
 		}
-	}
+	}*/
 	logger.Debugf("BlockService.BlockFetched %s", c)
 	return blk, nil
 }
